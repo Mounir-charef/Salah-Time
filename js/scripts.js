a6781efcebe9c766f47009123c1af89a064b5428
Mounir <polomnr1@gmail.com>
2022-09-03 18:27:11 +0100

    Done2

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,12 @@ const getPrayer = async (position) => {
 
         long = position.coords.longitude;
         lat = position.coords.latitude;
-        const [respTodoOne, respTodoTwo] = await Promise.all([
-            fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long} &apiKey=64ff5742184c40e69b04e0312190ff01`),
-            fetch(`http://api.aladhan.com/v1/calendar?latitude=${lat}&longitude=${long}&method=2`)
+        const [respTodoOne] = await Promise.all([
+            fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long} &apiKey=64ff5742184c40e69b04e0312190ff01`)
+            // fetch(`http://api.aladhan.com/v1/calendar?latitude=${lat}&longitude=${long}&method=2`)
         ]);
         const cityData = await respTodoOne.json();
-        const prayerData = await respTodoTwo.json();
+        // const prayerData = await respTodoTwo.json();
 
         let time = document.createElement('h3');
         time.textContent = d.toLocaleTimeString();
@@ -30,18 +30,18 @@ const getPrayer = async (position) => {
         },1000)
 
         city = cityData['features'][0]['properties']['city'];
-        prayer = prayerData.data[0].timings;
         cityElemeny.textContent = city;
-        for(let key in prayer){
-            const li = document.createElement("li");
-            const span = document.createElement("span");
-            li.classList.add('prayer-time','new-li');
-            span.classList.add('pray')
-            li.textContent = key;
-            span.textContent = prayer[key];
-            li.appendChild(span);
-            listPrayer.appendChild(li);
-        }
+        // prayer = prayerData.data[0].timings;
+        // for(let key in prayer){
+        //     const li = document.createElement("li");
+        //     const span = document.createElement("span");
+        //     li.classList.add('prayer-time','new-li');
+        //     span.classList.add('pray')
+        //     li.textContent = key;
+        //     span.textContent = prayer[key];
+        //     li.appendChild(span);
+        //     listPrayer.appendChild(li);
+        // }
 
     } catch (err) {
         throw err;
